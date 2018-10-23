4ed53ab34a51ca96f8c3d8230b2dc40231c69821
RafalJDev <32623268+RafalJDev@users.noreply.github.com>
2018-10-23 15:48:54 +0200

    Update brightness_controller.sh
    Reading displays name from system and choosing display with regex --- so now this script should work on other computers

--- brightness_controller.sh
+++ brightness_controller.sh
@@ -1,5 +1,12 @@
-display_number=$1; #1/2
+displayNumber=$1; #1/2
 brightness_direction=$2; #up/down
+
+  #choosing display 
+  var=$(xrandr --verbose | grep '.*\sconnected' | awk '{print $1}' ); 
+  chooseGroupFromRegex () {
+    gawk 'match($0,/'$1'/, ary) {print ary['${2:-'$displayNumber'}']}'; 
+  }  
+  displayName=$(echo $var | chooseGroupFromRegex '(.+)\s(.+)' $displayNumber) 
 
 
 if [ $brightness_direction = "u" ]; then
@@ -8,7 +15,7 @@ elif [ $brightness_direction = "d" ]; then
 		brightness_direction="down"
 fi
 
-sed_line=$display_number"p";
+sed_line=$displayNumber"p";
 
 echo "here"
 echo $sed_line;
@@ -17,10 +24,11 @@ brightness_initial=$(xrandr --verbose | grep Brightness | sed -n $sed_line | gre
 
 echo $brightness_initial;
 
-brightness_change=0.10;
 if [ $brightness_direction = "up" ]; then
+	brightness_change=0.10;
 	brightness_output=`echo $brightness_initial+$brightness_change | bc`;
 elif [ $brightness_direction = "down" ]; then
+	brightness_change=0.06;
 	brightness_output=`echo $brightness_initial-$brightness_change | bc`;
 else
 	echo "Bad direction: " $brightness_direction;
@@ -37,10 +45,10 @@ fi
 echo $brightness_output;
 
 
-# gamma_current=$(xrandr --verbose | grep Gamma | grep -o '[0-9][0-9:.]*' | sed -n $sed_line)
-# gamma_current="1.0:1.0:1.0"
+ gamma_current=$(xrandr --verbose | grep Gamma | grep -o '[0-9][0-9:.]*' | sed -n $sed_line)
+ # gamma_current="1.0:1.0:1.0"
 
-# echo "gamma: " $gamma_current
+echo "gamma: "$gamma_current
 
-display_name=DP-$display_number
-xrandr --output $display_name --brightness $brightness_output --gamma 1.0:1.0:0.8;
+xrandr --output $displayName --brightness $brightness_output --gamma 1.0:1.0:0.9;
+# 1.0:1.0:0.8
