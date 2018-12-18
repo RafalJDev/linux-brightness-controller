2f996fcd16e72f497824dad7b85569b6a5b9889b
RafalJDev <32623268+RafalJDev@users.noreply.github.com>
2018-12-18 14:45:44 +0100

    Updating script

--- brightness_controller.sh
+++ brightness_controller.sh
@@ -1,40 +1,32 @@
+#!/bin/bash
 displayNumber=$1; #1/2
-brightness_direction=$2; #up/down
+brightnessDirection=$2; #up/down
+yellowLevel=$3; #0.0 to 1.0
 
-  #choosing display 
-  var=$(xrandr --verbose | grep '.*\sconnected' | awk '{print $1}' ); 
-  chooseGroupFromRegex () {
-    gawk 'match($0,/'$1'/, ary) {print ary['${2:-'$displayNumber'}']}'; 
-  }  
-  displayName=$(echo $var | chooseGroupFromRegex '(.+)\s(.+)' $displayNumber) 
+if [ -z $yellowLevel ]; then
+	yellowLevel=0.5;
+fi
 
-
-if [ $brightness_direction = "u" ]; then
-	brightness_direction="up"
-elif [ $brightness_direction = "d" ]; then
-		brightness_direction="down"
+if [ $brightnessDirection = "u" ]; then
+	brightnessDirection="up"
+elif [ $brightnessDirection = "d" ]; then
+	brightnessDirection="down"
 fi
 
 sed_line=$displayNumber"p";
 
-echo "here"
-echo $sed_line;
-
 brightness_initial=$(xrandr --verbose | grep Brightness | sed -n $sed_line | grep -o '[0-9.]*');
 
-echo $brightness_initial;
-
-if [ $brightness_direction = "up" ]; then
+if [ $brightnessDirection = "up" ]; then
 	brightness_change=0.10;
 	brightness_output=`echo $brightness_initial+$brightness_change | bc`;
-elif [ $brightness_direction = "down" ]; then
-	brightness_change=0.06;
+elif [ $brightnessDirection = "down" ]; then
+	brightness_change=0.07;
 	brightness_output=`echo $brightness_initial-$brightness_change | bc`;
 else
-	echo "Bad direction: " $brightness_direction;
+	echo "Bad direction: " $brightnessDirection;
 fi
 
-echo $brightness_output;
 
 if [[ "$(echo "${brightness_output} > 1.00" | bc)" -eq 1 ]]; then
 	brightness_output=1.00;
@@ -42,13 +34,16 @@ elif [[ "$(echo "${brightness_output} < 0.00" | bc)" -eq 1 ]]; then
 	brightness_output=0.1;
 fi
 
-echo $brightness_output;
-
-
- gamma_current=$(xrandr --verbose | grep Gamma | grep -o '[0-9][0-9:.]*' | sed -n $sed_line)
- # gamma_current="1.0:1.0:1.0"
+gamma_current=$(xrandr --verbose | grep Gamma | grep -o '[0-9][0-9:.]*' | sed -n $sed_line)
+# gamma_current="1.0:1.0:1.0"
 
 echo "gamma: "$gamma_current
 
-xrandr --output $displayName --brightness $brightness_output --gamma 1.0:1.0:0.9;
+var=$(xrandr --verbose | grep '.*\sconnected' | awk '{print $1}' ); 
+chooseGroupFromRegex () {
+	gawk 'match($0,/'$1'/, ary) {print ary['${2:-'$displayNumber'}']}'; 
+}
+displayName=$(echo $var | chooseGroupFromRegex '(.+)\s(.+)' $displayNumber) 
+
+xrandr --output $displayName --brightness $brightness_output --gamma 1.0:1.0:$yellowLevel;
 # 1.0:1.0:0.8
